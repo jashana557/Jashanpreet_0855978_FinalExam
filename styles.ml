bb436925da87957a4fc2046c5c3748b9df122c4e
jashana557 <161057460+jashana557@users.noreply.github.com>
2024-04-26 11:42:49 -0400

    Add files via upload

---
+++ styles.ml
@@ -0,0 +1,25 @@
+header {
+     color:#43abf9
+	 padding:12px;
+	 
+}
+nav {
+     text-align:Right;
+	 color:#43abf9;
+	 font-family:Arial,sans-serif;
+}
+
+Section
+{
+     color:#f9f301;
+	 background color:#01f918;
+}
+
+container
+{
+     padding:15px;
+}
+
+body{
+     background color: #0134f9;
+}
